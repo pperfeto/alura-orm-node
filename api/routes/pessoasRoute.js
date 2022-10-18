e72e34b0199fc8d0db1110eac5ce5d4c965b7a88
pperfeto <paulo.perfetto@gmail.com>
2022-10-17 23:06:07 -0300

    commit

---
+++ api/routes/pessoasRoute.js
@@ -0,0 +1,12 @@
+const { Router } = require('express');
+const PessoaController = require('../controllers/PessoaController');
+
+const router = Router();
+
+router.get('/pessoas', PessoaController.pegaTodasAsPessoas);
+router.get('/pessoas/:id', PessoaController.pegaUmaPessoa);
+router.post('/pessoas', PessoaController.criaPessoa);
+router.put('/pessoas', PessoaController.atualizaPessoa);
+router.delete('/pessoas', PessoaController.apagaPessoa);
+
+module.exports = router;
