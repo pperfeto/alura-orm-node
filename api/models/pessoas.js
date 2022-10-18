e0e1467008340e613ba9ce0f401d888a5d1de910
pperfeto <paulo.perfetto@gmail.com>
2022-10-17 23:21:14 -0300

    commit

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -10,7 +10,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Pessoas.hasMany(models.Turmas);
+      Pessoas.hasMany(models.Matriculas);
     }
   }
   Pessoas.init({
