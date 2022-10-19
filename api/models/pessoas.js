9543f3a99b21e6d84bce371f221985b032686899
pperfeto <paulo.perfetto@gmail.com>
2022-10-18 23:16:13 -0300

    validacao do campo nome

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -14,7 +14,14 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   Pessoas.init({
-    nome: DataTypes.STRING,
+    nome: {
+      type: DataTypes.STRING,
+      validate: {
+        funcaoValidadora: function(dado) {
+          if (dado.length < 3) throw new Error('Campo nome deve ter mais de 3 caracteres.');
+        }
+      }
+    },
     ativo: DataTypes.BOOLEAN,
     email: {
       type: DataTypes.STRING,
